9dc4383dc682159f8558e23dd7090e15906e1737
Holger Mueller <9jholger@gmail.com>
2021-07-08 13:14:28 -0500

    worked on store

--- src/store/trackHours/index.js
+++ src/store/trackHours/index.js
@@ -9,6 +9,26 @@ export default {
     SET_HOURS(state, payload) {
       state.userHours = payload;
     },
+
+    ADD_HOURS(state, payload) {
+      const hoursToEdit = state.userHours.find((theseHours) => {
+        return theseHours.id === payload.hoursId;
+      });
+
+      if (payload.hoursToAdd) {
+        hoursToEdit.userHours = payload.hoursToAdd;
+      }
+    },
+
+    REMOVE_HOURS(state, payload) {
+      const hoursToEdit = state.userHours.find((theseHours) => {
+        return theseHours.id === payload.hoursId;
+      });
+
+      if (payload.hoursToRemove) {
+        hoursToEdit.userHours = payload.hoursToRemove;
+      }
+    },
   },
 
   actions: {
@@ -18,12 +38,27 @@ export default {
       firebase
         .collection("userHoursToMakeUp")
         .where("creatorId", "==", getters.user.userId)
-        .onSnapshot((querySnapshot) => {
-          let userHours = null;
-          querySnapshot.forEach((doc) => {
-            console.log;
-          });
-        });
+        .onSnapshot(
+          (querySnapshot) => {
+            let userHours = [];
+            querySnapshot.forEach((doc) => {
+              console.log(doc.data());
+
+              let hoursToDom = {
+                hoursId: doc.id,
+                hoursLeft: doc.data().userHours,
+                creatorId: doc.data().creatorId,
+              };
+              userHours.push(hoursToDom);
+            });
+            commit("SET_HOURS", userHours);
+            commit("SET_LOADING", false);
+          },
+          (err) => {
+            commit("SET_LOADING", true);
+            commit("SET_ERROR", err);
+          }
+        );
     },
 
     setInitialHoursToMakeUp({ commit, getters }) {
@@ -44,16 +79,17 @@ export default {
         });
     },
 
-    addHours({ commit, getters }, payload) {
+    addHours({ commit }, payload) {
       commit("SET_LOADING", true);
 
       firebase
         .collection("userHoursToMakeUp")
-        .where("creatorId", "==", getters.user.userId)
+        .doc(payload.hoursId)
+        .update({
+          userHours: payload.hoursToAdd,
+        })
         .then(() => {
-          let hoursToUpdate = "";
-          let hoursToAdd = payload;
-
+          commit("ADD_HOURS", payload);
           commit("SET_LOADING", false);
         })
         .catch((err) => {
@@ -62,13 +98,17 @@ export default {
         });
     },
 
-    removeHours({ commit, getters }, payload) {
+    removeHours({ commit }, payload) {
+      commit("SET_LOADING", true);
+
       firebase
         .collection("userHoursToMakeUp")
-        .where("creatorId", "==", getters.user.userId)
+        .doc(payload.hoursId)
+        .update({
+          userHours: payload.hoursToRemove,
+        })
         .then(() => {
-          let hoursToUpdate = "";
-          let hoursToRemove = payload;
+          commit("REMOVE_HOURS", payload);
 
           commit("SET_LOADING", false);
         })

af84a1b6e7b8b8cb25f1722e0955ef0c0dd6e358
Holger Mueller <9jholger@gmail.com>
2021-07-14 21:11:04 -0500

    began rebuilding store

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -13,9 +13,29 @@ export default {
   },
 
   actions: {
-    registerUser({ commit }) {
+    registerUser({ commit }, payload) {
       commit("SET_LOADING", true);
       commit("CLEAR_ERROR");
+
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(payload.email, payload.password)
+        .then((userCredential) => {
+          let user = userCredential.user;
+
+          let newUser = {
+            displayName: payload.displayName,
+            email: user.email,
+            userId: user.uid,
+          };
+
+          commit("SET_USER", newUser);
+          commit("SET_LOADING", false);
+        })
+        .catch((err) => {
+          commit("SET_LOADING", true);
+          commit("SET_ERROR", err);
+        });
     },
 
     loginUser({ commit }) {
@@ -23,7 +43,15 @@ export default {
       commit("CLEAR_ERROR");
     },
 
-    autoSignIn() {},
+    autoSignIn({ commit }, payload) {
+      commit("SET_LOADING", false);
+
+      commit("SET_USER", {
+        userId: payload.uid,
+        email: payload.email,
+        displayName: payload.displayName,
+      });
+    },
 
     logout({ commit }) {
       commit("SET_LOADING", true);

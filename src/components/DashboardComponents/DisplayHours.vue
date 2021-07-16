db4a3291c23a805eedcdecd325dfab3374f92ff7
Holger Mueller <9jholger@gmail.com>
2021-07-15 19:39:35 -0500

    added progress bar to card

--- src/components/DashboardComponents/DisplayHours.vue
+++ src/components/DashboardComponents/DisplayHours.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    <v-card class="hours-card text-center">
+    <v-card class="hours-card text-center" :loading="loading">
+      <template slot="progress">
+        <v-progress-linear indeterminate color="blue" height="10">
+        </v-progress-linear>
+      </template>
+
       <v-card-title class="heading2">
         <v-icon left>mdi-account-circle</v-icon>Hello,
       </v-card-title>
@@ -21,6 +26,12 @@ export default {
   name: "HoursDisplay",
 
   props: {},
+
+  computed: {
+    loading() {
+      return this.$store.getters.loading;
+    },
+  },
 };
 </script>
 
